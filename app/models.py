07b71ea86b63b7a51cbf4ba353906fc34a3f5253
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-12-21 23:45:10 +0530

    back populate relationship added

--- app/models.py
+++ app/models.py
@@ -1,22 +1,25 @@
+from sqlalchemy.orm import relationship
 from app import db
 from flask_login import UserMixin
 
 
 # CONFIGURE TABLE
+class Users(UserMixin, db.Model):
+    __tablename__ = "registered_users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(100))
+    posts = relationship("BlogPost", back_populates="author")
+
+
 class BlogPost(db.Model):
+    __tablename__="blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("registered_users.id"))
+    author = relationship("Users", back_populates="posts")
     img_url = db.Column(db.String(250), nullable=False)
-
-
-class Users(UserMixin, db.Model):
-    __tablename__ = "registered_users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    password = db.Column(db.String(100))

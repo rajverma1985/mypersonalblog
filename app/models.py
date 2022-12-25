cbbc6f323dd9e49d8020378a508c25ba0e3e2de3
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-12-26 01:01:20 +0530

    adding FK constraints and relationship between tables

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(100))
     posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
 
 
 class BlogPost(db.Model):
@@ -23,9 +24,14 @@ class BlogPost(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey("registered_users.id"))
     author = relationship("Users", back_populates="posts")
     img_url = db.Column(db.String(250), nullable=False)
+    comments = relationship("Comment", back_populates="parent_post")
 
 
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
+    auth_id = db.Column(db.Integer, db.ForeignKey("registered_users.id"))
+    comment_author = db.relationship("Users", back_populates="comments")
+    parent_post = relationship("BlogPost", back_populates="comments")
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
